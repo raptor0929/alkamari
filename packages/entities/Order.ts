c7b09afe8bbf0141a955a23b417803d5b35574eb
raptor0929 <flaura.dev@gmail.com>
2024-08-11 12:29:34 -0300

    Fix bugs

--- packages/entities/Order.ts
+++ packages/entities/Order.ts
@@ -1,12 +1,13 @@
 import { OrderStatus } from "~~/types/types";
 
 export type Order = {
-    id: string;
-    anounceId: string;
-    fromWalletAddress: string;
-    toWalletAddress: string;
-    orderSize: number;
-    Fee: number;
-    status: OrderStatus;
-    correlationId: number;
-  };
+  id: string;
+  anounceId: string;
+  fromWalletAddress: string;
+  toWalletAddress: string;
+  orderSize: number;
+  Fee: number;
+  status: OrderStatus;
+  correlationId: number;
+  documentId: string;
+};
